12a3f00e4a73b07c0530919cb0140935a1aac436
MktRDPrinter <mkt@rdprinter.com>
2025-01-31 10:33:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function obtenerCotizacion() {
+  fetch('https://dolarapi.com/v1/ambito/dolares/oficial')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("compra").textContent = `$${data.compra}`;
+      document.getElementById("venta").textContent = `$${data.venta}`;
+    })
+    .catch(error => console.error("Error al obtener la cotización:", error));
+}
+
+// Ejecutar la función al cargar la página
+obtenerCotizacion();
+
+// Actualizar cada 60 segundos (1 minuto)
+setInterval(obtenerCotizacion, 60000);
